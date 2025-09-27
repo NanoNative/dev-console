36f36461465af8fbd9c587bf8171c1034c07f320
ab <basuabhi92@gmail.com>
2025-09-27 13:27:11 +0200

    Added tests for configs

--- src/test/java/org/nanonative/ab/devconsole/service/DevConsoleServiceTest.java
+++ src/test/java/org/nanonative/ab/devconsole/service/DevConsoleServiceTest.java
@@ -7,13 +7,15 @@ import org.nanonative.nano.services.http.HttpServer;
 import org.nanonative.nano.services.http.model.ContentType;
 import org.nanonative.nano.services.http.model.HttpMethod;
 import org.nanonative.nano.services.http.model.HttpObject;
-import org.nanonative.nano.services.logging.LogService;
 
 import java.util.Map;
 
 import static org.nanonative.ab.devconsole.service.DevConsoleService.BASE_URL;
 import static org.nanonative.ab.devconsole.service.DevConsoleService.CONFIG_DEV_CONSOLE_URL;
+import static org.nanonative.ab.devconsole.service.DevConsoleService.DEFAULT_MAX_EVENTS;
+import static org.nanonative.ab.devconsole.service.DevConsoleService.DEFAULT_MAX_LOGS;
 import static org.nanonative.ab.devconsole.service.DevConsoleService.DEFAULT_UI_URL;
+import static org.nanonative.ab.devconsole.service.DevConsoleService.DEV_CONFIG_URL;
 import static org.nanonative.ab.devconsole.service.DevConsoleService.DEV_EVENTS_URL;
 import static org.nanonative.ab.devconsole.service.DevConsoleService.DEV_INFO_URL;
 import static org.nanonative.ab.devconsole.service.DevConsoleService.DEV_LOGS_URL;
@@ -50,7 +52,7 @@ class DevConsoleServiceTest {
     @Test
     void fetchLogTest() {
         String log = "Test log output";
-        final Nano nano = new Nano(new HttpServer(), new DevConsoleService(), new HttpClient(), new LogService());
+        final Nano nano = new Nano(new HttpServer(), new DevConsoleService(), new HttpClient());
         nano.context(DevConsoleServiceTest.class).info(() -> log);
         final HttpObject result = new HttpObject()
             .methodType(HttpMethod.GET)
@@ -59,6 +61,43 @@ class DevConsoleServiceTest {
         assertThat(result.statusCode()).isEqualTo(200);
         assertThat(result.hasContentType(ContentType.APPLICATION_JSON));
         assertThat(result.bodyAsString()).contains(log);
+    }
+
+    @Test
+    void fetchConfigTest() {
+        final Nano nano = new Nano(new HttpServer(), new DevConsoleService(), new HttpClient());
+        final HttpObject result = new HttpObject()
+            .methodType(HttpMethod.GET)
+            .path(serverUrl + nano.service(HttpServer.class).port() + BASE_URL + DEV_CONFIG_URL)
+            .send(nano.context(DevConsoleServiceTest.class));
+        assertThat(result.statusCode()).isEqualTo(200);
+        assertThat(result.hasContentType(ContentType.APPLICATION_JSON));
+        assertThat(result.bodyAsString()).contains("maxEvents").contains("maxLogs").contains("baseUrl");
+    }
+
+    @Test
+    void updateConfigTest() {
+        final String newBaseUrl = "/tests";
+        final Integer newMaxLogs = 101;
+        final DevConsoleService devConsoleService = new DevConsoleService();
+        final Nano nano = new Nano(new HttpServer(), devConsoleService, new HttpClient());
+        final String baseUrl = devConsoleService.basePath;
+        final Integer maxLogs = devConsoleService.maxLogs;
+
+        final HttpObject result = new HttpObject()
+            .methodType(HttpMethod.POST)
+            .body(Map.of("baseUrl", newBaseUrl, "maxLogs", newMaxLogs))
+            .path(serverUrl + nano.service(HttpServer.class).port() + BASE_URL + DEV_CONFIG_URL)
+            .send(nano.context(DevConsoleServiceTest.class));
+
+        assertThat(result.statusCode()).isEqualTo(200);
+        assertThat(result.hasContentType(ContentType.APPLICATION_JSON));
+        assertThat(result.bodyAsString()).contains(newBaseUrl).contains(String.valueOf(newMaxLogs));
+        assertThat(baseUrl).isEqualTo(DEFAULT_UI_URL);
+        assertThat(maxLogs).isEqualTo(DEFAULT_MAX_LOGS);
+        assertThat(devConsoleService.basePath).isEqualTo(newBaseUrl);
+        assertThat(devConsoleService.maxLogs).isEqualTo(newMaxLogs);
+        assertThat(devConsoleService.maxEvents).isEqualTo(DEFAULT_MAX_EVENTS);
     }
 
     @Test
@@ -71,15 +110,15 @@ class DevConsoleServiceTest {
         assertThat(result.statusCode()).isEqualTo(200);
         assertThat(result.hasContentType(ContentType.TEXT_HTML));
         assertThat(DevConsoleService.STATIC_FILES.size()).isEqualTo(4);
-        assertThat(result.bodyAsString()).contains("\uD83D\uDE80 Nano Developer Console");
+        assertThat(result.bodyAsString()).contains("<!DOCTYPE html>");
     }
 
     @Test
     void fetchHtmlUsingCustomUrlTest() {
-        final String customPath = "/nano";
+        final String customPath = "/ab";
         final Nano nano = new Nano(Map.of(
             CONFIG_DEV_CONSOLE_URL, customPath
-        ),new HttpServer(), new DevConsoleService(), new HttpClient());
+        ), new HttpServer(), new DevConsoleService(), new HttpClient());
         final HttpObject result = new HttpObject()
             .methodType(HttpMethod.GET)
             .path(serverUrl + nano.service(HttpServer.class).port() + BASE_URL + customPath)
@@ -87,6 +126,6 @@ class DevConsoleServiceTest {
         assertThat(result.statusCode()).isEqualTo(200);
         assertThat(result.hasContentType(ContentType.TEXT_HTML));
         assertThat(DevConsoleService.STATIC_FILES.size()).isEqualTo(4);
-        assertThat(result.bodyAsString()).contains("\uD83D\uDE80 Nano Developer Console");
+        assertThat(result.bodyAsString()).contains("<!DOCTYPE html>");
     }
 }
